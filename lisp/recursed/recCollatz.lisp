55daed30d6b4d1888985fd0332fb515784af509e
Michael W. Skarbek <skarbek_mw@cobra.cs.mercer.edu>
2021-12-08 17:55:00 -0500

    Complete and commented

--- lisp/recursed/recCollatz.lisp
+++ lisp/recursed/recCollatz.lisp
@@ -1,12 +1,13 @@
 #!/usr/bin/sbcl --script
 
-
+;; Defines the struct for an item to be placed in an array
 (defstruct item
     itemNumber
     itemSequenceLen
 )
 
 
+;; Returns an array of ten 'Items' with -1 for both values
 (defun prepArr()
     (let
         (
@@ -30,7 +31,7 @@
     ) ; end of let space
 )
 
-
+;; Calcualtes the sequence length of the input number
 (defun calcCollatz(inNum)
     (let
         (
@@ -48,7 +49,9 @@
     ) ; end let
 )
 
-
+;; Updates the sequence array using an insertion sort
+;; 'Items' are ranked based upon the length of their collatz sequence length
+;; Array is organized lowest -> highest
 (defun updateSequence(lengthArr inItem)
     (let
         (
@@ -58,14 +61,17 @@
             (arrItem)
         ); end vars
 
+        ;; iterates through the array backwards
         (dotimes (i 10)
             (setf arrItem (aref lengthArr (- 9 i) ) )
-            ;(print arrItem)
-            ;(print inItem)
             (setf difference (- (item-itemSequenceLen inItem) (item-itemSequenceLen arrItem)))
             (setf magnitude (- (item-itemNumber inItem) (item-itemNumber arrItem)))
 
 
+            ;; When the input sequence is greater,
+            ;; shift all items down and place the new item at
+            ;; the location
+            ;; insert the new item
             (when (> difference 0)
                 (dotimes (j (- 9 i))
                         (setf (aref lengthArr j) (aref lengthArr (+ j 1)))
@@ -73,23 +79,33 @@
                 (setf (aref lengthArr (- 9 i)) inItem)
                 (return-from updateSequence lengthArr)
             ) ; end when
+
+
+            ;; The next two statemnts keep the item with the
+            ;; lowest integer when they have the same sequence lenght
+
             (when (and (= difference 0) (> magnitude 0))
                 (return-from updateSequence lengthArr)
             ); end when
+
             (when (or (= difference 0) (< magnitude 0))
                 (setf (aref lengthArr i) inItem)
                 (return-from updateSequence lengthArr)
             ); end when
 
-
         ); end do times
 
 
+    ;; Return the updated array
     (return-from updateSequence lengthArr)
 
     ); end let
 ) ; End function updateSequence
 
+
+;; Uses an insertion sort to arrange the magArray
+;; based on the values of the integers
+;; takes the sequence array as input
 
 (defun updateMagnitude(seqArr)
     (let
@@ -99,15 +115,21 @@
             (magItem)
         ); end vars
 
+        ;; Go through the sequence array backwards
         (dotimes (x 10)
 
             (setf seqItem (aref seqArr (- 9 x)))
 
+
+            ;; Using a block allows for easy exiting of the next loop
             (block magLoop
 
+            ;; Find approporiate spot in the magArray going backwards
             (dotimes (i 10)
                 (setf magItem (aref magArr (- 9 i) ) )
                 
+                ;; Shift all items down and place the held
+                ;; sequence item at the location
                 (when (> (item-itemNumber seqItem) (item-itemNumber magItem) )
                     (dotimes (j (- 9 i))
                             (setf (aref magArr j) (aref magArr (+ j 1)))
@@ -126,7 +148,7 @@
     ); end let
 )
 
-
+;; Allows for taking cpommand line arguments
 (defun argv ()
 
     ( or
@@ -164,20 +186,34 @@
 
 (setf lengthArr (prepArr) )
 
-
+;; Run for the entirety of the range input 
 (loop 
-    (setq seqLen (calcCollatz counter))
-    (setq itemHold (make-item :itemNumber counter :itemSequenceLen seqLen))
+    (setq seqLen (calcCollatz counter)) ; get the collatz sequence length and store it
+    (setq itemHold (make-item :itemNumber counter :itemSequenceLen seqLen)) ; make an item object
     
-    
+    ;Update the sequence array
     (setq lengthArr (updateSequence lengthArr itemHold))
-    (when (= counter endNum) (return nil) )
-    (incf counter)
+
+
+    (when (= counter endNum) (return nil) ) ; Exit when the end has been reached
+    (incf counter)                          ; Else increment and contininue
 )
 
+; Update the magnitude array
 (setf magnitudeArr (updateMagnitude lengthArr))
 
-(print lengthArr)
-(terpri)
-(print magnitudeArr)
-(terpri)
+
+;;Format print the arrays
+
+(format t  "Sorted by sequence length ~%")
+(dotimes (i 10)
+    (format t "~D ~D" (item-itemNumber (aref lengthArr (- 9 i) )) (item-itemSequenceLen(aref lengthArr (- 9 i))) )
+    (terpri)
+) ; end loop
+
+(format t "~%")
+(format t  "Sorted by integer size ~%")
+(dotimes (i 10)
+    (format t "~D ~D" (item-itemNumber (aref magnitudeArr (- 9 i) )) (item-itemSequenceLen(aref magnitudeArr (- 9 i))) )
+    (terpri)
+) ; end loop
